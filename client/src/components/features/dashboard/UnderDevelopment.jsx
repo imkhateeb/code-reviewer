f389b4d88373c7d0db460507d2ad69f48e84bc8c
imkhateeb <luckykhateeb2@gmail.com>
2024-12-24 17:18:07 +0530

    [Feat]: All required features developed

--- client/src/components/features/dashboard/UnderDevelopment.jsx
+++ client/src/components/features/dashboard/UnderDevelopment.jsx
@@ -1,7 +1,9 @@
 const UnderDevelopment = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      UnderDevelopment
+    <div className="w-full h-screen max-md:h-[85vh] flex flex-col items-center justify-center">
+      <div className="text-xl bold-poppins max-md:text-lg">
+        This page is under development.
+      </div>
     </div>
   );
 };
